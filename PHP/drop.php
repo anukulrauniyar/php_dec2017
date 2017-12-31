9d9bb24ac569021b578cacfc28c55fa71728ac20
dharmendra321 <dharmendra321.singh@gmail.com>
2017-12-31 14:26:03 +0530

    31DEC

--- PHP/drop.php
+++ PHP/drop.php
@@ -5,6 +5,11 @@ function abc(v)
 {
   self.location="drop.php?cntry="+v;
 }
+function abcd(s)
+{
+  var v=document.getElementById("cn").value;
+  self.location="drop.php?cntry="+v+"&stat="+s;
+}
 </script>
 </head>
 <body>
@@ -13,15 +18,24 @@ $country=array("INDIA","CHINA","JAPAN");
 $state=array("INDIA"=>array("UP","MP","BIHAR"),
 			 "CHINA"=>array("CP","CP2","BIHAR2"),
 			 "JAPAN"=>array("UP3","MP3","BIHAR3"));
+$city=array("UP"=>array("UP-C","UP-D","UP-E"),
+			 "CP"=>array("CP-C","CP-D","CP-E"),
+			 "MP3"=>array("MP-C","MP-D","MP-E"));
+
 $cntry="";
+$stat="";
 if(isset($_GET['cntry']))
 {
 	$cntry=$_GET['cntry'];
 }
+if(isset($_GET['stat']))
+{
+	$stat=$_GET['stat'];
+}
 
 ?>
 <form >
-Select Country<select name="cnt" Onchange="abc(this.value)">
+Select Country<select name="cnt" Onchange="abc(this.value)" id="cn">
 		<option value=" ">Select a Value</option>
 		<?
 		foreach($country as $v)
@@ -34,7 +48,7 @@ Select Country<select name="cnt" Onchange="abc(this.value)">
 		?>
 	</select><br>
 Select State
-<select name="sta">
+<select name="sta" Onchange="abcd(this.value)">
 		<option value=" ">Select a Value</option>
 		<?
 		if(isset($_GET['cntry']))
@@ -42,7 +56,26 @@ Select State
 		  $v=$state[$cntry];
 		  foreach($v as $data)
 			{
-			echo "<option value='$data'>".$data."</option>";
+			echo "<option value='$data' ";
+		 if($stat==$data)
+			 echo" SELECTED " ;
+		 echo " >".$data."</option>";
+		  }
+		}
+		?>
+	</select>
+	<br>
+Select State
+<select name="city">
+		<option value=" ">Select a Value</option>
+		<?
+		if(isset($_GET['stat']))
+		{
+		  $v=$city[$stat];
+		  foreach($v as $data)
+			{
+			echo "<option value='$data' ";
+			echo " >".$data."</option>";
 		  }
 		}
 		?>
